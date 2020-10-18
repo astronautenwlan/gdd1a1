92adc8dbfb997c28c5e24d0c578c41c2388386c0
astronautenwlan <a.reiterer@student.tugraz.at>
2020-10-18 22:34:15 +0200

    First Enemy Shooting

---
+++ Ningenerica Shuriken Warrior/Assets/Scripts/Dark_Wizard_Weapon_Shooting_Script.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dark_Wizard_Weapon_Shooting_Script : MonoBehaviour
+{
+    private WeaponShootingScript weapon;
+
+    void Awake()
+    {
+        weapon = GetComponent<WeaponShootingScript>();
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((weapon != null) && weapon.canAttack())
+        {
+            weapon.doAttack(false);
+        }
+    }
+}
