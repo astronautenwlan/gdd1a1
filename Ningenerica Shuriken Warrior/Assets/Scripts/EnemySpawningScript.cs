1ba963fe77b6200cc25eee70f289ad61f1145062
astronautenwlan <a.reiterer@student.tugraz.at>
2020-10-22 22:41:01 +0200

    adding changes before resetting IDE

--- Ningenerica Shuriken Warrior/Assets/Scripts/EnemySpawningScript.cs
+++ Ningenerica Shuriken Warrior/Assets/Scripts/EnemySpawningScript.cs
@@ -6,6 +6,7 @@ public class EnemySpawningScript : MonoBehaviour
 {
     // This is our class to spawn all enemies.
     // We will use the same code as in WeaponShootingScript
+    // The enemy prefabs needs to be filled in unity to choose any number of enemy prefabs to spawn
     public Transform[] enemy_prefabs_;
     public float spawning_speed_seconds_ = 1.5f;
     public float spawning_speed_multiplier_ = 1.0f;
@@ -40,7 +41,18 @@ public class EnemySpawningScript : MonoBehaviour
     //spawn enemies directly with this script
     private void spawnRandomEnemy()
     {
+        //random number between 0 and array size-1
+        // the Random.Range returns an int from min (inclusive) to max (exclusive!)
+        // so we don't need to apply -1 to get our array access
+        int enemy_picked_from_array_ = Random.Range(0, enemy_prefabs_.Length);
         
+        //instantiate the randomly chosen prefab
+        var random_enemy_created = Instantiate(enemy_prefabs_[enemy_picked_from_array_]);
+        
+
+        //second random float between +3 and -2
+        //move that prefab to y = 0 + random number, and x = 10
+
     }
     
 }
