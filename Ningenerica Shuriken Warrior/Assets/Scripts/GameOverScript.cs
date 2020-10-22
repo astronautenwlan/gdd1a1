4fcf2be0049da5378a1b56ee8eeca481f3680ecc
astronautenwlan <a.reiterer@student.tugraz.at>
2020-10-23 01:38:39 +0200

    Merged Gameplay and Title Scene, working on Game-Over Menu

---
+++ Ningenerica Shuriken Warrior/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+    private Button[] buttons_;
+
+    void Awake()
+    {
+        buttons_ = GetComponentsInChildren<Button>();
+        hideButtons();
+    }
+
+    public void hideButtons()
+    {
+        foreach (var b in buttons_)
+        {
+            b.gameObject.SetActive(false);
+        }
+    }
+
+    public void showButtons()
+    {
+        foreach (var b in buttons_)
+        {
+            b.gameObject.SetActive(true);
+        }
+    }
+
+    public void exitToMenu()
+    {
+        SceneManager.LoadScene("TitleMenuScene");
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene("GameplayScene");
+    }
+
+
+// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
