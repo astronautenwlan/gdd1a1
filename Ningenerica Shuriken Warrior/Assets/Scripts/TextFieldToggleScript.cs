216cab0670577826e3b4e0b91e28a39daa910d61
astronautenwlan <a.reiterer@student.tugraz.at>
2020-10-23 06:24:16 +0200

    Added Title Screen Credits Toggle Button

---
+++ Ningenerica Shuriken Warrior/Assets/Scripts/TextFieldToggleScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFieldToggleScript : MonoBehaviour
+{
+    public bool is_hidden_ = false;
+
+    void Awake()
+    {
+        if (is_hidden_ == true)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+    
+}
---
+++ Ningenerica Shuriken Warrior/Assets/Scripts/TitleToggleCreditsScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class TitleToggleCreditsScript
+    : MonoBehaviour
+{
+    private Text[] sub_fields_;
+
+    private void Awake()
+    {
+        sub_fields_ = GetComponentsInChildren<Text>();
+    }
+
+    public void toggleMenuCredits()
+    {
+        foreach (Text t in sub_fields_)
+        {
+            TextFieldToggleScript toggle = t.GetComponent<TextFieldToggleScript>();
+            if (toggle != null)
+            {
+                if (toggle.is_hidden_)
+                {
+                    t.gameObject.SetActive(true);
+                    toggle.is_hidden_ = false;
+                }
+                else
+                {
+                    t.gameObject.SetActive(false);
+                    toggle.is_hidden_ = true;
+                }
+            }
+
+        }
+    }
+}
